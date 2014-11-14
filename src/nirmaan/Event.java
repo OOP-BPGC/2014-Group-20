dbf7a7d07c512c1bca89f996ba54988aca72bd7e
pamitm <pamitm@gmail.com>
2014-11-14 21:05:32 +0530

    Updated event

--- src/nirmaan/Event.java
+++ src/nirmaan/Event.java
@@ -11,6 +11,7 @@ public class Event implements Serializable {
 	Date endDate;
 	ArrayList<Activity> activities;
 	ArrayList<Person> participants;
+	private boolean approved;
 	public class Activity implements Serializable {
 		String name;
 		int numStudents;
@@ -37,13 +38,50 @@ public class Event implements Serializable {
 	public void addParticipants(Person person) {
 		participants.add(person);
 	}
+	public static  void approve(Event e) {
+		e.approved = true;
+	}
+	public boolean checkApproval() {
+		return this.approved;
+	}
 	public void save() {
 		ArrayList<Event> events;
 		events = (ArrayList<Event>)Utility.deserialize("events.ser");
 		if(events == null) {
 			events = new ArrayList<Event>();
 		}
-		events.add(this);
+		boolean flag = true;
+		for(Event e :events)
+        {         
+            if(!(this.startDate.before(e.startDate)&&this.endDate.before(e.startDate)||this.startDate.after(e.endDate)&&this.endDate.after(e.endDate)))
+            flag=false;
+        }
+        if(flag) {
+        	events.add(this);
+        }
 		Utility.serialize(events, "events.ser");
 	}
+	 public static ArrayList<Event> getPending()
+	    {
+	        ArrayList<Event> event = new ArrayList<Event>();
+	        event = (ArrayList<Event>)Utility.deserialize("Events.ser");
+	        ArrayList<Event> temp = new ArrayList<Event>();
+	       try
+	       {for(Event tr : event)
+	        {   if(!tr.checkApproval())
+	            {
+	                temp.add(tr);
+	            }
+	                
+	        }
+	       }
+	       catch ( NullPointerException e)
+	       {
+	           
+	       }
+	       
+	        return temp;
+	        
+	        
+	    }
 }
