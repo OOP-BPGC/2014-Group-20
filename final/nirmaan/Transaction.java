89b2a1ced76afd13644ba430e6e968b5cbf55518
pamitm <pamitm@gmail.com>
2014-11-21 16:59:17 +0530

    Updated Transaction
    No errors anymore!

--- final/nirmaan/Transaction.java
+++ final/nirmaan/Transaction.java
@@ -19,7 +19,7 @@ public class Transaction implements Serializable{
     private String reason;
     private double amount;
     private static double funds;
-    private boolean approved;
+    public boolean approved;
     public Transaction(String userName,String reason,double amount)
     {
         this.userName=userName;// while creating a transaction in the main code pass the member object's 
@@ -28,7 +28,7 @@ public class Transaction implements Serializable{
         this.approved=false;
             }
     
-    private void modifyFunds()
+   public void modifyFunds()
     {   
         if(this.approved)
         {	
@@ -37,48 +37,50 @@ public class Transaction implements Serializable{
             
         }
     }
-   
-    public static boolean approveTransaction(Transaction E)
+   public double getFunds() {
+	   return funds;
+   }
+   //Gives ConcurrentModificationException sometimes
+    public static void approveTransaction(Transaction E)
     {
-    	if(E.approved == false) {
-    	ArrayList<Transaction> trans;
-        trans = (ArrayList<Transaction>)Utility.deserialize("Transactions.ser");
-        if(trans==null)
-        {
-        	System.out.println("here");
-        return false;
-        }
-        try
-        
-        {
-        	boolean rem = false;
-        	for(Transaction tran:trans) {
-        		if(E.getuser().equals(tran.getuser())&& E.getreason().equals(tran.getreason()) && E.getamount()==tran.getamount())
-        		rem = trans.remove(tran);
-            }
-        		
-        	
-        E.approved=true;
-        System.out.println(E.approved+ "  " + rem);
-        E.modifyFunds();
-        trans.add(E);
-        Utility.serialize(trans, "Transactions.ser");
-        return true;
-        }
-        catch(Exception e)
-        {
-        	e.printStackTrace();
-        	return false;
-            
-                    }
-        
-        
     	
-            }
-    	else {
-    		System.out.println("here1");
-    		return false;
-    	}
+	    	ArrayList<Transaction> trans;
+	    	ArrayList<Transaction> temp = new ArrayList<Transaction>();
+	        trans = (ArrayList<Transaction>)Utility.deserialize("Transactions.ser");
+	        if(trans==null)
+	        {
+	        	return;
+	        }
+	        try
+	        
+	        {
+	        	
+	        	for(Transaction tran:trans) {
+	        		if(E.getuser().equals(tran.getuser())&& E.getreason().equals(tran.getreason()) && E.getamount()==tran.getamount()){
+	        			
+	            }
+	        	else{
+	        		
+	        		temp.add(tran);
+	        	}
+	        		
+	        	}
+	        E.approved=true;
+	      
+	        E.modifyFunds();
+	        temp.add(E);
+	        Utility.serialize(temp, "Transactions.ser");
+	        return;
+	        }
+	        catch(Exception e)
+	        {
+	            return;
+	                    }
+	        
+	        
+	    	
+	    
+    	
     }
     public String getuser()
     {
@@ -139,7 +141,7 @@ public class Transaction implements Serializable{
         {   if(!tr.checkapproval())
             {
                 temp.add(tr);
-               
+                
             }
                 
         }
@@ -154,13 +156,13 @@ public class Transaction implements Serializable{
     }
     
     public static void main(String[] args) {
-    	Transaction one = new Transaction("swx","swx",33);
-    //one.registertransaction();
+    	Transaction one = new Transaction("sux","sux",33);
+    	//one.registertransaction();
     	one.printall();
     	ArrayList<Transaction> pend = Transaction.getPending();
     	//System.out.println(pend.get(0).getuser());
     	System.out.println(pend.size());
-    	System.out.println(Transaction.approveTransaction(one));
+    	Transaction.approveTransaction(pend.get(0));
     	one.printall();
     	
     	/*pend = Transaction.getPending();
