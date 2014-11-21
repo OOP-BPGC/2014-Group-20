8d1cac0989c26263cf3ee1c9ebfa530755098b69
jimbrutan <f2013038@goa.bits-pilani.ac.in>
2014-11-21 21:18:59 +0530

    Activity.java
    activity as a class needs to be used in jsp files
    hence moving the nested class to a seperate file

---
+++ Submission-4/code/src/nirmaan/Activity.java
@@ -0,0 +1,43 @@
+package nirmaan;
+
+
+import java.io.Serializable;
+import java.util.Date;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ *
+ * @author sreejith
+ */
+public class Activity implements Serializable{
+    Date date;
+    String name;
+    public Activity(String name, Date d)
+    {
+        this.date=d;
+        this.name=name;
+    }
+    public void setdate(Date d)
+    {
+        this.date=d;
+    }
+    public void setname(String name)
+    {
+        this.name=name;
+    }
+    public Date getdate()
+    {
+        return this.date;
+    }
+    public String getname()
+    {
+        return this.name;
+    }
+}
+
+         
+    
