15b6ff6a9c58cdca3aa8a2600a762ae48691e223
jimbrutan <f2013038@goa.bits-pilani.ac.in>
2014-11-22 14:13:51 +0530

    Update Calendar.java
    added functionality of sorting and returning only approved events

--- Submission-4/code/src/nirmaan/Calendar.java
+++ Submission-4/code/src/nirmaan/Calendar.java
@@ -5,27 +5,74 @@ import java.util.ArrayList;
 import nirmaan.Event.Activity;
 
 /**
- * @author Kartik
  *
+ * @author sreejith
  */
 public class Calendar {
-	
-	/**
-	 * @return Schedule of events
-	 */
-	public String smallCalendar() {
-		ArrayList<Event> events;
-		events = (ArrayList<Event>)Utility.deserialize("events.ser");
-		String calendar = "";
-		if (events != null) {
-			for(Event event: events) {
-				calendar = calendar + event.name + " from "  + event.startDate.toString()  
-						+ " to "  + event.endDate.toString() + "\n Activities:";
-				for(Activity activity: event.activities) {
-					calendar = calendar + activity.name + " on " + activity.date.toString();
-				}
-			}
-		}
-		return calendar;
-	}
+    private ArrayList<Event> finalised;
+    /**
+     * returns a sorted and approved list of events
+     **/
+     
+    public ArrayList<Event> ShowCalendar()
+    {   Date order[]=new Date[30];
+        ArrayList<Event> a =new ArrayList<Event>();
+        int i=0;
+        finalised=(ArrayList<Event>)Utility.deserialize("/home/sreejith/NetBeansProjects/WebApplication1/src/java/src/Events.ser");
+        for (Event e: finalised)
+        { 
+           order[i++]=e.getStartDate();
+        }
+       
+       
+        Date temp=new Date();
+               int min= 0;
+        for (i=0;i<finalised.size();i++)
+        {   min=i;
+            for(int j=i;j<finalised.size();j++)
+            {
+                if(order[j].before(order[min]))
+                {
+                    min=j;
+                }
+            }
+            temp=order[i];
+            order[i]=order[min];
+            order[min]=temp;
+            
+        } 
+         
+        for(i=0;i<finalised.size();)
+        {
+            for(Event ab : finalised )
+            {
+            if(ab.getStartDate().equals(order[i]))
+            {
+             a.add(ab);
+             i++;
+            }
+            }
+        }
+        finalised=new ArrayList<Event>();
+        for(Event ab : a)
+        {
+            /*System.out.println("Event Name : " + ab.getEvent()+"    StartDate :  "+ab.getStartDate() + " End Date : " + ab.getEndDate());
+            System.out.println("........Activities include.........");
+            ArrayList<Activity> n = ab.showallactivity();
+            for(Activity act : n)
+            {
+                System.out.println(act.getname() + " " + act.getdate());
+            }*/
+            if(ab.checkapproval())
+                finalised.add(ab);
+            
+        }
+        
+        return finalised;
+    }
+   /*public static void main(String args[])
+    {   System.out.println("..........................Upcoming Events......................");
+        Calendar a= new Calendar();
+        a.ShowCalendar();
+    }*/
 }
